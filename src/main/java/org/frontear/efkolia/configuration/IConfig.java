8ece6b1091faee52424763f3074671fef288b764
Ali Rizvi <frontear4594@gmail.com>
2019-12-13 17:48:33 -0500

    Fix typos in config
    This gives far greater flexibility in how information is given, but still provides a fixed standard.

--- src/main/java/org/frontear/efkolia/configuration/IConfig.java
+++ src/main/java/org/frontear/efkolia/configuration/IConfig.java
@@ -4,17 +4,18 @@ import org.frontear.internal.NotNull;
 
 /**
  * The interface which defines a config service. This config service must be able to load
- * information from files and parse them into objects, specifically {@link }. It must register the
- * objects that require modification, and modify them whenever the config file is loaded.
+ * information from files and parse them into objects, specifically {@link IConfigurable}. It must
+ * register the objects that require modification, and modify them whenever the config file is
+ * loaded.
  */
-public interface IConfig {
+public interface IConfig<E> {
     /**
      * Registers an object for configuration. This object will have its internal state modified
      * anytime {@link IConfig#load()} is run.
      *
      * @param object The object to register to the configuration
      */
-    void register(@NotNull final Object object);
+    void register(@NotNull final IConfigurable<? extends E> object);
 
     /**
      * Removes the registration of an object from being configured. The object will no longer be
@@ -22,16 +23,16 @@ public interface IConfig {
      *
      * @param object The object to register to the configuration
      */
-    void unregister(@NotNull final Object object);
+    void unregister(@NotNull final IConfigurable<? extends E> object);
 
     /**
      * Attempts to read the configuration file into the program, then iterate on all {@link
-     * IConfig#register(Object)} elements to modify their internal state.
+     * IConfig#register(IConfigurable)} elements to modify their internal state.
      */
     void load();
 
     /**
-     * Saves the internal state of all {@link IConfig#register(Object)} elements to the
+     * Saves the internal state of all {@link IConfig#register(IConfigurable)} elements to the
      * configuration file.
      */
     void save();
