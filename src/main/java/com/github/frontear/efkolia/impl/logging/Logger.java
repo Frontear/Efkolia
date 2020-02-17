090a1b83f2377ed007a38bbfcb466046f8cda98a
Ali Rizvi <frontear4594@gmail.com>
2020-02-17 07:08:31 -0500

    Update Logger to create the Minecraft configuration
    Since we totally overwrite the original config, it can potentially break areas of Minecraft, which we don't want.

--- src/main/java/com/github/frontear/efkolia/impl/logging/Logger.java
+++ src/main/java/com/github/frontear/efkolia/impl/logging/Logger.java
@@ -1,12 +1,16 @@
 package com.github.frontear.efkolia.impl.logging;
 
-import static org.apache.logging.log4j.Level.*;
+import static org.apache.logging.log4j.Level.ALL;
+import static org.apache.logging.log4j.Level.DEBUG;
 
 import com.github.frontear.efkolia.Properties;
 import com.github.frontear.efkolia.api.logging.ILogger;
-import com.github.frontear.internal.*;
+import com.github.frontear.internal.NotNull;
+import com.github.frontear.internal.Nullable;
+import java.io.IOException;
 import lombok.val;
-import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.ConsoleAppender.Target;
 import org.apache.logging.log4j.core.config.Configurator;
@@ -15,16 +19,22 @@ import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFact
 public final class Logger implements ILogger {
     private static final LoggerContext context;
 
+    // generates the default Minecraft config
     static {
         val builder = ConfigurationBuilderFactory.newConfigurationBuilder();
         {
-            builder.setStatusLevel(Level.WARN);
+            builder.setStatusLevel(Level.WARN).setPackages("com.mojang.util");
 
             val console = builder.newAppender("SysOut", "Console");
             console.addAttribute("target", Target.SYSTEM_OUT);
             console.add(builder.newLayout("PatternLayout")
                 .addAttribute("pattern", "[%d{HH:mm:ss}] [%t/%level] [%logger]: %msg%n"));
             builder.add(console);
+
+            val queue = builder.newAppender("ServerGuiConsole", "Queue");
+            queue.add(builder.newLayout("PatternLayout")
+                .addAttribute("pattern", "[%d{HH:mm:ss}] [%t/%level] [%logger]: %msg%n"));
+            builder.add(queue);
 
             val file = builder.newAppender("File", "RollingRandomAccessFile");
             file.addAttribute("fileName", "logs/latest.log")
@@ -37,8 +47,11 @@ public final class Logger implements ILogger {
             builder.add(file);
 
             val root = builder.newRootLogger(DEBUG);
+            root.add(builder.newFilter("MarkerFilter", "DENY", "NEUTRAL")
+                    .addAttribute("marker", "NETWORK_PACKETS"));
             root.add(builder.newAppenderRef("SysOut"))
-                .add(builder.newAppenderRef("File").addAttribute("level", ALL));
+                .add(builder.newAppenderRef("File").addAttribute("level", ALL))
+                .add(builder.newAppenderRef("ServerGuiConsole"));
             builder.add(root);
         }
 
