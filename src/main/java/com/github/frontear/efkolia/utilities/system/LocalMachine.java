50a06942fdef0635d84598bf5a1bd7e25d95f132
Ali Rizvi <frontear4594@gmail.com>
2020-02-26 21:45:59 -0500

    Fix bug in propertyCheck
    It was actually checking if the properties were changed in the jvm args

--- src/main/java/com/github/frontear/efkolia/utilities/system/LocalMachine.java
+++ src/main/java/com/github/frontear/efkolia/utilities/system/LocalMachine.java
@@ -3,7 +3,12 @@ package com.github.frontear.efkolia.utilities.system;
 import static com.github.frontear.efkolia.utilities.system.OperatingSystem.*;
 import static com.github.frontear.efkolia.utilities.system.SystemArchitecture.*;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.*;
+import java.io.IOException;
 import java.lang.management.ManagementFactory;
+import java.net.*;
+import java.nio.file.*;
 import java.util.function.Function;
 import lombok.*;
 import lombok.experimental.UtilityClass;
@@ -23,7 +28,7 @@ public class LocalMachine {
             .noneMatch(x -> x.startsWith(
                 "-D" + property + "=")); // checks if a property explicitly changed in the jvm args.
 
-        if (!propertyCheck.apply("os.name") && !propertyCheck.apply("os.arch")) {
+        if (propertyCheck.apply("os.name") && propertyCheck.apply("os.arch")) {
             val os_name = System.getProperty("os.name");
             val os_arch = System.getProperty("os.arch");
 
@@ -35,6 +40,41 @@ public class LocalMachine {
         else {
             throw new UnsupportedOperationException(
                 "Properties \"os.name\" and/or \"os.arch\" have been overwritten in the jvm arguments");
+        }
+    }
+
+    /**
+     * Grabs a {@link Path} based on a file url. It will automatically format path separators based on the operating system
+     * @param path The path to the file. This <b>must</b> use unix style path separators, they will be internally changed to reflect the correct system
+     * @return An instance of {@link Path} pointing to the file
+     */
+    @SneakyThrows(URISyntaxException.class)
+    public Path getFile(@NonNull final String path) {
+        val path_url = equalsOS(WINDOWS) ? path.replace('/', '\\') : path;
+
+        return Paths.get(new URI(path));
+    }
+
+    /**
+     * Attempts to grab the local clipboard data, and return it as a string.
+     * @return Clipboard data as a {@link String}
+     */
+    @SneakyThrows({ UnsupportedFlavorException.class, IOException.class })
+    public String getClipboard() {
+        val toolkit = Toolkit.getDefaultToolkit();
+
+        return (String) toolkit.getSystemClipboard().getData(DataFlavor.stringFlavor);
+    }
+
+    /**
+     * Sets the local clipboard to a custom string, discarding any old data.
+     * @param content The new clipboard text
+     */
+    public void setClipboard(@NonNull final String content) {
+        val toolkit = Toolkit.getDefaultToolkit();
+        if (!content.isEmpty()) {
+            val string = new StringSelection(content);
+            toolkit.getSystemClipboard().setContents(string, string);
         }
     }
 
