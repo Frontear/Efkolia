d0600695b93e36da1b2d2b2a58f956d2a2856d6e
Ali Rizvi <frontear4594@gmail.com>
2020-03-01 15:55:15 -0500

    Allow Loader to return after init
    This could be a success code or boolean, or even something more.

--- src/main/java/com/github/frontear/efkolia/api/loader/ILoaderMod.java
+++ src/main/java/com/github/frontear/efkolia/api/loader/ILoaderMod.java
@@ -6,14 +6,18 @@ import com.github.frontear.internal.Nullable;
 /**
  * The common interface that defines the java file that loads the entire mod and its functionality
  * Treat it like it is your main class, the entry-point for the entire program.
+ *
+ * @param <T> The type you wish to return after instantiation.
  */
-public interface ILoaderMod {
+public interface ILoaderMod<T> {
     /**
      * The initialization of the mod. This is where the mod should begin loading its functionality.
      * It should be treated like the main method of a class, an entry-point for your mod. This
      * should ideally be setting up your {@link IMinecraftMod}, and nothing more.
      *
      * @param args The starting arguments for the application.
+     *
+     * @return A type which will represent whether successful initialization occurred
      */
-    void init(@Nullable String... args);
+    T init(@Nullable String... args);
 }
